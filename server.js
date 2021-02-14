96f9e41c2e1da326d13419db2fea68d84f7bc0c1
Vishrut8898 <vishrut9869@gmail.com>
2021-02-14 17:28:58 +0530

    'initial'

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = process.env.PORT || 3030;
+app.use(express.static('public'));
+app.use(express.json());
+
+const connectDB = require('./config/db');
+connectDB();
+
+// Template Engine
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+app.use('/api/files', require('./routes/files.js'));
+app.use('/files', require('./routes/show'));
+app.use('/files/download', require('./routes/download'));
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+});
